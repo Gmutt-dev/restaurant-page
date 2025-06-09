5d759cff01fd2654acaf496ea5b78556c68a5185
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-06-09 10:38:50 +0200

    Added eventlistener on nav with handlers
    To show correct page when clicking on a nav button.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import "./styles.css";
 import displayMain from "./main.js";
+import displayMenu from "./menu.js";
+import displayAbout from "./about.js";
 
-displayMain();
+const nav = document.querySelector("nav");
+nav.addEventListener("click", e => {
+    const content = document.querySelector("#content");
+    content.innerHTML = "";
+    const navButtons = document.querySelectorAll("nav > button");
+    navButtons.forEach(button => button.classList.remove("selected-button"));
+    e.target.classList.add("selected-button");
+    switch (e.target.id) {
+        case "nav-home":
+            displayMain();
+            break;
+        case "nav-menu":
+            displayMenu();
+            break;
+        case "nav-about":
+            displayAbout();
+            break;
+    }
+})
